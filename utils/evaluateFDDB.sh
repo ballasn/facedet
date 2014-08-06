5549304462c9df105620ebd6e532d9bdbcc14275
antoine-cha <antoine.chassang0@gmail.com>
2014-08-06 15:14:12 -0400

    [add] command I used to apply evaluate

---
+++ utils/evaluateFDDB.sh
@@ -0,0 +1,1 @@
+./evaluate2 -a /data/lisa/data/faces/FDDB/FDDB-folds/FDDB-fold-01-ellipseList.txt -d ./orderedOutput.txt -f 0 -i /data/lisa/data/faces/FDDB/ -l ../../FDDB_files_lists/FDDB-fold-01.txt -r ./
