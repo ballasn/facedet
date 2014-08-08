fb0a6223ea4c6dc762f4a4942eb2faca2f8af247
antoine-cha <antoine.chassang0@gmail.com>
2014-08-08 17:12:51 -0400

    -s to display results

--- utils/evaluateFDDB.sh
+++ utils/evaluateFDDB.sh
@@ -1,3 +1,3 @@
 # I removed the last file of the list, that 's why I'm pointing to a file list in my dir
 # The corresponding file doesn't appear in our data
-./evaluate2 -a /data/lisa/data/faces/FDDB/FDDB-folds/FDDB-fold-01-ellipseList.txt -d ./orderedOutput.txt -f 0 -i /data/lisa/data/faces/FDDB/ -l ../../FDDB_files_lists/FDDB-fold-01.txt -r ./
+./evaluate2 -a /data/lisa/data/faces/FDDB/FDDB-folds/FDDB-fold-01-ellipseList.txt -d ./orderedOutput.txt -f 0 -i /data/lisa/data/faces/FDDB/ -l ../../FDDB_files_lists/FDDB-fold-01.txt -r ./ -s
