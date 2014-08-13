5bd0e72de2023295c59399588bb2bd7158d06fc8
antoine-cha <antoine.chassang0@gmail.com>
2014-08-13 17:39:48 -0400

    faster for negatives with more rnd box drawn

--- utils/create_dataset.py
+++ utils/create_dataset.py
@@ -2,6 +2,7 @@ import sys
 import os
 import cv2
 import numpy as np
+from time import time
 
 RED = (0, 0, 255)
 
@@ -122,48 +123,72 @@ def create_negative_sample(img_list,
                            patch_size,
                            img_dir,
                            bbox_dir,
+                           path,
                            bbox_ext = '.txt',
                            nb_channels = 3):
 
-    nb_draw = 3
+    nb_draw = 100
 
-    ### Initialize resulting matrix
+    # Initialize resulting matrix
     data = np.zeros((nb_samples, patch_size[0] * patch_size[1] * nb_channels), dtype=np.float32)
 
     cur = 0
+    chkpt_print = 0
+    chkpt_save = 0
+    nb_file = 0
+    t1 = 0
+    t2 = 0
+    t0 = time()
     while cur < nb_samples:
-        if cur%100 == 0:
-            sys.stdout.write("\r"+str(cur))
+        if cur % 100 == 0 and cur != 0 and cur != chkpt_print:
+            chkpt_print = cur
+            t1 = time()
+            sys.stdout.write("\r"+str(cur)+", last 100 in "+str(t1-t2)+" s"+
+            ", total : "+ str(nb_file) +" files or "+
+            str(float(t1-t0)/float(chkpt_print))+" s/image")
             sys.stdout.flush()
+            t2 = time()
 
+        if cur % 10000 == 0 and cur != chkpt_save:
+            np.save(path, data)
+            chkpt_save = cur
+            print "temp save :", cur, "examples"
+            print ""
 
         idx = np.random.randint(0, len(img_list))
         img = img_list[idx]
-        print cur, img, nb_samples
+        # print cur, img, nb_samples
 
-        ### Get image filenames
+        # Get image filenames
         filepath = os.path.join(img_dir, img).strip()
         bbox_file = os.path.join(bbox_dir, os.path.splitext(img)[0] + bbox_ext)
 
-        ### Pursue only if the image has a bounding box
+        # Pursue only if the image has a bounding box
         if (not os.path.isfile(bbox_file)):
             continue
 
-        ### Load data
+        # Load data
         img = cv2.imread(filepath)
-        if img == None:
+        nb_file += 1
+        if img is None:
             print "Loading failed: [%s]" % filepath
-            exit (1)
-        bbox = np.loadtxt(skipper(bbox_file), delimiter = ',', dtype=int)
+            exit(1)
+        # Load face areas
+        bbox = np.loadtxt(skipper(bbox_file), delimiter=',', dtype=int)
         if len(bbox.shape) == 1:
             bbox = np.reshape(bbox, (1, bbox.shape[0]))
 
         for i in xrange(0, nb_draw):
+            # Draw a random box
             sample_box = rnd_bounding_box(img.shape[0], img.shape[1])
 
             overlap = False
             for b in xrange(0, bbox.shape[0]):
-                overlap = overlap or bbox_overlap(sample_box[0, :], bbox[b, :])
+                # Check if isn't overlapping a face
+                if bbox_overlap(sample_box[0, :], bbox[b, :]):
+                    overlap = True
+                    break
+            # Draw a new box
             if overlap:
                 continue
 
@@ -179,10 +204,11 @@ def create_negative_sample(img_list,
             # cv2.imshow('image', img)
             # cv2.waitKey(0)
             # cv2.destroyAllWindows()
-
-
-            face = img[sample_box[0, 2]:sample_box[0, 2] + sample_box[0, 4], sample_box[0, 3]:sample_box[0, 3] + sample_box[0, 5]]
-            face = cv2.resize(face, patch_size, interpolation = cv2.INTER_CUBIC)
+            # Define the data as pixels inside the valid_box
+            valid_box = img[sample_box[0, 2]:sample_box[0, 2] + sample_box[0, 4], sample_box[0, 3]:sample_box[0, 3] + sample_box[0, 5]]
+            # Resize box to fit <patch_size>
+            valid_box = cv2.resize(valid_box, patch_size, interpolation =
+                    cv2.INTER_LINEAR)
 
             ### Debug show images
             # cv2.imshow('image', face)
@@ -190,13 +216,22 @@ def create_negative_sample(img_list,
             # cv2.destroyAllWindows()
 
             ### Save the patch
-            print cur, data.shape
-            data[cur, :] = face.reshape(1, patch_size[0] * patch_size[1] * nb_channels)
+            #print cur, data.shape
+            data[cur, :] = valid_box.reshape(1, patch_size[0] * patch_size[1] * nb_channels)
             cur += 1
             if cur >= nb_samples:
                 break
 
     return data
+
+def positivesFromDataset(googledataset):
+    """
+    Returns a nparray containing the images of faces
+    The argument is GoogleDataset object
+    """
+    for i in xrange(len(googledataset)):
+        # Returns a facedatasetExample
+        img = googledataset[i]
 
 
 
@@ -226,10 +261,27 @@ if __name__ == '__main__':
         exit(1)
     patch_size= (size, size)
 
+## Counting bboxes
+    """
+    nb_box = 0
+    bbox_ext = ".txt"
+    for img in img_files:
+        if nb_box%100 == 0:
+            sys.stdout.write("\r"+str(nb_box)+" bboxes")
+            sys.stdout.flush()
+        bbox_file = os.path.join(bbox_dir, os.path.splitext(img)[0] + bbox_ext)
+        print bbox_file
+        if (os.path.isfile(bbox_file)):
+            # print "Load:", bbox_file
+            bbox = np.loadtxt(skipper(bbox_file), delimiter=',')
+            nb_box += bbox.shape[0]
+    print nb_box
+    """
+####
     if (negative > 0):
         print "Negatif"
         data = create_negative_sample(img_files, negative, patch_size,
-                                     img_dir, bbox_dir)
+                                     img_dir, bbox_dir, sys.argv[6])
     else:
         print "Positif"
         data = create_positives_sample(img_files, patch_size,
