ea81d35771e75de831dfdb0f7c46a9b3b77a3ef7
antoine-cha <antoine.chassang0@gmail.com>
2014-08-29 17:11:10 -0400

    setting framework for cascade training

--- utils/train_cascade.py
+++ utils/train_cascade.py
@@ -12,19 +12,24 @@ except ImportError:  # Antoine's config
     from datasets.faceDataset_cascade import faceDataset
 
 
-def train_one_stage(model_yaml, dataset=None):
+def train_one_stage(model_yaml, dataset=None, model_file=None):
     """
     Train a stage of the cascade
     Return the path to the best model.
     """
     with open(model_yaml, "r") as m_y:
         model = yaml_parse.load(m_y)
-    print "Loaded first YAML"
+    print "Loaded YAML"
     #############################
-    model.algorithm.termination_criterion._max_epochs = 200
+    model.algorithm.termination_criterion._max_epochs = 1000
     #############################
     # Write info about the model
-    model_file = model.extensions[0].save_path
+    if model_file is None:
+        model_file = model.extensions[0].save_path
+    else:
+        model.extensions[0].save_path = model_file + "_best.pkl"
+        model.save_path = model_file + ".pkl"
+
     if dataset is not None:
         # We use the modified dataset
         model.dataset = dataset
@@ -74,34 +79,35 @@ def define_inactives(model_file, dataset, acc_prob):
 
 if __name__ == '__main__':
 
-    positives = "/data/lisatmp3/chassang/facedet/16/pos16_100_eq.npy"
-    negatives = "/data/lisatmp3/chassang/facedet/16/neg16_100_eq.npy"
+    # Define parameters
+    positives = "/data/lisatmp3/chassang/facedet/16/pos16_700_eq.npy"
+    negatives = "/data/lisatmp3/chassang/facedet/16/neg16_700_eq.npy"
     ratio = 0.8
+    acc_prob = 0.1
     # which_set = 'test' return all examples ???
-    dataset = faceDataset(positives, negatives, 'coucou')
+    dataset = faceDataset(positives, negatives, 'train')
     batch_size = 128
-    batches = dataset.iterator(mode='negative_seq',
-                               batch_size=batch_siize)
-    # We skip the first training for convenience
-    m_yaml = "../exp/simple_net_multidim.yaml"
-    model_file = '../exp/sn_1kiter_best.pkl'
+    m_yaml = "../exp/simple_net_extended.yaml"
+    for i in range(3):
+        # Train one stage
+        m_file = train_one_stage(m_yaml, dataset=dataset,
+                                 model_file="cascade"+str(i))
 
-    # Get the list ofi uninteresting negatives
-    inactives = define_inactives(model_file, dataset, 0.05)
-    print 0.05, len(inactives)
-    inactives = np.array(inactives)
-    np.save("inactives.npy", inactives)
+        # Get the list of uninteresting negatives
+        inactives = define_inactives(m_file+"_best.pkl", dataset, acc_prob)
+        print 0.05, len(inactives)
+        inactives = np.array(inactives)
+        np.save("inactives"+str(i)+".npy", inactives)
+        if i != 0:
+            past_inactives = np.load("inactives"+str(i-1)+".npy")
+            new_inactives = np.concatenate((past_inactives, inactives), axis=0)
+            new_inactives = np.unique(new_inactives)
+            np.save("inactives"+str(i)+".npy", new_inactives)
 
-    # Define the new dataset without the inactives
-    dataset_bis = faceDataset(positives, negatives, 'train',
-            inactive_examples="inactives.npy")
+        # Define the new dataset without the inactives
+        dataset = faceDataset(positives, negatives, 'train',
+                        inactive_examples="inactives"+str(i)+".npy")
 
-    # Train a second stage on the modified dataset
-    m_file_bis = train_one_stage(m_yaml, dataset_bis)
-
-    inactives2 = define_inactives(predict, dataset, 0.05)
-    print 0.05, len(inactives)
-    print 0.05, len(inactives2)
 
 
 
