febb2760ec01c3cacd36898a88079df37e53ff25
Nicolas Ballas <ballas.n@gmail.com>
2014-10-06 17:47:34 -0400

    Add dsn2 experiments

--- utils/create_dataset/compute_mean.py
+++ utils/create_dataset/compute_mean.py
@@ -5,6 +5,7 @@ import numpy as np
 from time import time
 import numpy as np
 from random import shuffle
+from math import sqrt
 
 if __name__ == '__main__':
 
@@ -17,8 +18,13 @@ if __name__ == '__main__':
     neg = np.load(sys.argv[2])
 
     mean = (np.sum(pos, axis=0) + np.sum(neg, axis=0))/ float(pos.shape[0] +neg.shape[0])
-
     np.save(sys.argv[3], mean)
+
+    size = int(sqrt(pos[0].shape[0] / 3))
+    img_shape = [size, size, 3]
+    tmp = np.reshape(mean, img_shape)
+    cv2.imshow("mean ", np.asarray(tmp, dtype=np.uint8))
+    cv2.waitKey(0)
 
 
 
