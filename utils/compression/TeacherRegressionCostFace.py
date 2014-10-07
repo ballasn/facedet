ae81093b5340e6df12df4afaae27995477688b9f
Adriana Romero <adriana.romsor@gmail.com>
2014-10-07 16:04:18 -0400

    dataset & hints working

--- utils/compression/TeacherRegressionCostFace.py
+++ utils/compression/TeacherRegressionCostFace.py
@@ -39,12 +39,14 @@ class TeacherRegressionCost(DefaultDataSpecsMixin, Cost):
         # Compute student output
         Ps_y_given_x = model.fprop(x)
         
-        Ps_y_given_x = Ps_y_given_x.reshape(shape=(Ps_y_given_x.shape[0]*
+        Ps_y_given_x = Ps_y_given_x.reshape(shape=(Ps_y_given_x.shape[0],
                        Ps_y_given_x.shape[1]*
-                       Ps_y_given_x.shape[2],
+                       Ps_y_given_x.shape[2]*
                        Ps_y_given_x.shape[3]),ndim=2)
+                       
         # Compute cost
-        rval = -T.log(Ps_y_given_x)[T.arange(targets.shape[0]), targets]  
+	rval = -T.log(Ps_y_given_x)[T.arange(targets.shape[0]), targets]
+
         
         return rval
         
@@ -55,9 +57,9 @@ class TeacherRegressionCost(DefaultDataSpecsMixin, Cost):
                                                 
         # Compute teacher relaxed output
 	Pt_y_given_x_relaxed = self.teacher.fprop(x)
-        Pt_y_given_x_relaxed = Pt_y_given_x_relaxed.reshape(shape=(Pt_y_given_x_relaxed.shape[0]*
+        Pt_y_given_x_relaxed = Pt_y_given_x_relaxed.reshape(shape=(Pt_y_given_x_relaxed.shape[0],
 			       Pt_y_given_x_relaxed.shape[1]*
-			       Pt_y_given_x_relaxed.shape[2],
+			       Pt_y_given_x_relaxed.shape[2]*
 			       Pt_y_given_x_relaxed.shape[3]),ndim=2)	
 	
 
@@ -69,9 +71,9 @@ class TeacherRegressionCost(DefaultDataSpecsMixin, Cost):
         # Compute student relaxed output
         Ps_y_given_x_relaxed = model.fprop(x)
         
-        Ps_y_given_x_relaxed = Ps_y_given_x_relaxed.reshape(shape=(Ps_y_given_x_relaxed.shape[0]*
+        Ps_y_given_x_relaxed = Ps_y_given_x_relaxed.reshape(shape=(Ps_y_given_x_relaxed.shape[0],
 			       Ps_y_given_x_relaxed.shape[1]*
-			       Ps_y_given_x_relaxed.shape[2],
+			       Ps_y_given_x_relaxed.shape[2]*
 			       Ps_y_given_x_relaxed.shape[3]),ndim=2)	
                 
 	# Compute cost
